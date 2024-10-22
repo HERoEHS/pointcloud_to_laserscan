f0997f17f360eb8bdf62d3a7b4a16ed84d3c1bd8
HyeonguDO <dek0411@naver.com>
2024-10-22 13:02:39 +0900

    귤이에 맞는 launch 로 수정

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -10,39 +10,55 @@ def generate_launch_description():
             name='scanner', default_value='scanner',
             description='Namespace for sample topics'
         ),
-        Node(
-            package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
-            remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
-            parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
-            name='cloud_publisher'
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            arguments=[
-                '--x', '0', '--y', '0', '--z', '0',
-                '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
-                '--frame-id', 'map', '--child-frame-id', 'cloud'
-            ]
-        ),
+        # Node(
+        #     package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
+        #     remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
+        #     parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 5.0, 'cloud_size': 3000}],
+        #     name='cloud_publisher'
+        # ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_transform_publisher',
+        #     arguments=[
+        #         # '--x', '0', '--y', '0', '--z', '0',
+        #         # '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
+        #         '--frame-id', 'map', '--child-frame-id', 'odom'
+        #     ]
+        # ),
+        # # odom -> zed_camera_link 변환 퍼블리셔 추가
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_transform_publisher_odom_to_zed_camera_link',
+        #     arguments=[
+        #         # '--x', '0', '--y', '0', '--z', '0',
+        #         # '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
+        #         '--frame-id', 'odom', '--child-frame-id', 'zed_camera_link'
+        #     ]
+        # ),
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/cloud']),
-                        ('scan', [LaunchConfiguration(variable_name='scanner'), '/scan'])],
+            remappings=[
+                ('cloud_in', '/zed/zed_node/point_cloud/cloud_registered'),  # 입력 토픽 수정
+                ('scan', '/scan')  # 출력 토픽 수정
+            ],
             parameters=[{
-                'target_frame': 'cloud',
-                'transform_tolerance': 0.01,
-                'min_height': 0.0,
-                'max_height': 1.0,
-                'angle_min': -1.5708,  # -M_PI/2
-                'angle_max': 1.5708,  # M_PI/2
-                'angle_increment': 0.0087,  # M_PI/360.0
-                'scan_time': 0.3333,
-                'range_min': 0.45,
-                'range_max': 4.0,
+                # 'target_frame': 'zed_camera_link',  # 프레임 ID 수정
+                'target_frame': '', 
+                'transform_tolerance': 1.5,
+                'min_height': -0.4,
+                'max_height': 0.0,
+                'angle_min': -1.57,  # -M_PI/2 1.0472
+                'angle_max': 1.57,  # M_PI/2
+                'angle_increment': 0.02,  # M_PI/360.0
+                'scan_time': 0.1,
+                'range_min': 0.7,         # 원래 값 0.1
+                'range_max': 2.5,          # 7.0
                 'use_inf': True,
-                'inf_epsilon': 1.0
+                'inf_epsilon': 1.0,
+                'min_noise_distance': 0.5,          # 원래 1.0
+                # 'queue_size': 10000  # 큐 크기 증가
             }],
             name='pointcloud_to_laserscan'
         )
