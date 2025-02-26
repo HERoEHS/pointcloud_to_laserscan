a7c58b7ab8f39795d96807cd7d7bd325badd92f6
HyeonguDO <dek0411@naver.com>
2025-02-26 16:03:50 +0900

    라이다용, 제드용 코드 분리

--- launch/zed2i_pointcloud_to_laserscan_launch.py
+++ launch/zed2i_pointcloud_to_laserscan_launch.py
@@ -11,35 +11,26 @@ def generate_launch_description():
             description='Namespace for sample topics'
         ),
         
-        # Add the static transform from base_link to zed_camera_link
-        # Node(package='tf2_ros',
-        # executable='static_transform_publisher',
-        # name='static_tf_pub_base_link_to_zed_camera_link',
-        # arguments=['0.0', '0.0', '0.33', '0.0', '-0.1745', '0.0', 'base_link', 'zed_camera_link'],
-        # ),
-
         Node(
-            package='pointcloud_to_laserscan', executable='filtered_pointcloud_to_laserscan_node',
+            package='pointcloud_to_laserscan', executable='zed2i_pointcloud_to_laserscan_node',
             remappings=[
                 ('cloud_in', '/zed/zed_node/point_cloud/cloud_registered'),  # 입력 토픽 수정
                 ('scan', '/scan')  # 출력 토픽 수정
             ],
             parameters=[{
-                # 'target_frame': 'zed_camera_link',  # 프레임 ID 수정
                 'target_frame': '', 
                 'transform_tolerance': 1.5,
                 'min_height': -0.4,
-                'max_height': 0.0,
+                'max_height': 1.0,
                 'angle_min': -1.57,  # -M_PI/2 1.0472
                 'angle_max': 1.57,  # M_PI/2
                 'angle_increment': 0.02,  # M_PI/360.0
                 'scan_time': 0.1,
-                'range_min': 0.7,         # 원래 값 0.1
-                'range_max': 1.5,          # 7.0
+                'range_min': 0.3,         # 원래 값 0.1
+                'range_max': 3.0,          # 7.0
                 'use_inf': True,
                 'inf_epsilon': 1.0,
                 'min_noise_distance': 0.5,          # 원래 1.0
-                # 'queue_size': 10000  # 큐 크기 증가
             }],
             name='pointcloud_to_laserscan'
         )
